82c52595fa34d3934134daebc52d91f57a77ce38
root <damducthoai@gmail.com>
2018-01-12 09:57:00 +0700

    add download bat

---
+++ 0.download.bat
@@ -0,0 +1,4 @@
+git clone https://github.com/damducthoai/linkservice-frontend.git
+git clone https://github.com/damducthoai/linkservice-web.git
+git clone https://github.com/damducthoai/linkservice-common.git
+mvn clean package
